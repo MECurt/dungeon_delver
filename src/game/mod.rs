e75c39a3206da63bc4f4032b4c6edda7dae54634
Evan Curtis <m.e.curtis@protonmail.com>
2022-04-09 01:18:13 -0500

    restructured module file hierarchy

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,3 +1,5 @@
+mod room;
+
 pub enum GameResponse {
     OK,
     EXIT,
---
+++ src/game/room.rs
@@ -0,0 +1,8 @@
+pub struct Room {
+    id: u32
+}
+impl Room {
+    pub fn new(id: u32) -> Room {
+        Room {id}
+    }
+}
